ca2964d5ac1bb5554a0d68a19cf00afd26b3c5bf
FICHEKK <filipnemec3@gmail.com>
2019-12-06 19:00:31 +0100

    Registration finished! Login works, but needs some upgrading! Added lobby scene.

---
+++ Assets/Scripts/Lobby/InformationFiller.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Lobby {
+    public class InformationFiller : MonoBehaviour {
+        [SerializeField] private TMP_Text usernameText;
+        [SerializeField] private TMP_Text chipCountText;
+        [SerializeField] private TMP_Text winCountText;
+    
+        void Start() {
+            usernameText.text = PlayerPrefs.GetString("username");
+            chipCountText.text = PlayerPrefs.GetString("chipCount");
+            winCountText.text = PlayerPrefs.GetString("winCount");
+        }
+    }
+}
---
+++ Assets/Scripts/Login/LoginHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using TMPro;
+using UnityEngine;
+
+namespace Login {
+    public class LoginHandler : MonoBehaviour {
+        [SerializeField] private TMP_InputField usernameInputField;
+        [SerializeField] private TMP_InputField passwordInputField;
+        [SerializeField] private TMP_InputField addressInputField;
+        [SerializeField] private TMP_InputField portInputField;
+
+        private const int MinimumUsernameLength = 3;
+        private const int MinimumPasswordLength = 8;
+
+        public void Login() {
+            if (!IsLoginFormValid()) {
+                Debug.Log("Registration form is not valid.");
+                return;
+            }
+            
+            string address = addressInputField.text;
+            int port = int.Parse(portInputField.text);
+
+            using (TcpClient client = new TcpClient(address, port))
+            using (StreamReader reader = new StreamReader(client.GetStream()))
+            using (StreamWriter writer = new StreamWriter(client.GetStream())) {
+                writer.BaseStream.WriteByte((byte) ClientRequest.Login);
+                writer.WriteLine(usernameInputField.text);
+                writer.WriteLine(passwordInputField.text);
+                writer.Flush();
+
+                int responseCode = writer.BaseStream.ReadByte();
+                if (responseCode == -1) return;
+
+                ServerResponse response = (ServerResponse) responseCode;
+
+                if (response == ServerResponse.LoginSucceeded) {
+                    PlayerPrefs.SetString("username", usernameInputField.text);
+                    PlayerPrefs.SetString("chipCount", reader.ReadLine());
+                    PlayerPrefs.SetString("winCount", reader.ReadLine());
+                    
+                    GetComponent<SceneLoader>().LoadScene();
+                }
+                else {
+                    Debug.Log("Login failed: " + response);
+                }
+            }
+        }
+
+        private bool IsLoginFormValid() {
+            if (usernameInputField.text.Length < MinimumUsernameLength) return false;
+            if (passwordInputField.text.Length < MinimumPasswordLength) return false;
+            if (addressInputField.text == string.Empty) return false;
+            if (portInputField.text == string.Empty) return false;
+
+            return true;
+        }
+    }
+}
