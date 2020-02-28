96b58bdd074279e9413d4a0f10b9234674f3cc30
FICHEKK <filipnemec3@gmail.com>
2020-02-28 20:21:34 +0100

    Add visual chips. Add chip distribution calculator. Add basic poker sounds (not yet implemented in the game).

---
+++ Assets/Scripts/Table/StackCalculator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Random = UnityEngine.Random;
+
+namespace Table
+{
+    public static class StackCalculator
+    {
+        private const float MinStackCoverage = 0.5f;
+        private const string ChipSpritesPath = "Assets/Resources/Sprites/Chips";
+        private static readonly List<int> ChipValues = new List<int>();
+
+        static StackCalculator()
+        {
+            foreach (var fileInfo in new DirectoryInfo(ChipSpritesPath).GetFiles())
+            {
+                if(fileInfo.Name.EndsWith(".meta")) continue;
+
+                try
+                {
+                    ChipValues.Add(int.Parse(Path.GetFileNameWithoutExtension(fileInfo.Name)));
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+
+            ChipValues.Sort((i1, i2) => i2.CompareTo(i1));
+        }
+        
+        public static List<ChipValueAmountPair> CalculateChipDistribution(int stack)
+        {
+            if(stack <= 0) throw new ArgumentException("Stack must be greater than zero.");
+            
+            var chipDistribution = new List<ChipValueAmountPair>();
+
+            for(int index = 0; stack > 0; index++)
+            {
+                while (ChipValues[index] > stack) index++;
+
+                int amount = GetAmountFor(ChipValues[index], stack);
+                chipDistribution.Add(new ChipValueAmountPair(ChipValues[index], amount));
+                
+                stack -= ChipValues[index] * amount;
+            }
+            
+            chipDistribution.Reverse();
+            return chipDistribution;
+        }
+        
+        private static int GetAmountFor(int chipValue, int stack)
+        {
+            if (chipValue == 1) return stack;
+
+            int minAmount = (int) Math.Ceiling(stack * MinStackCoverage / chipValue);
+            int maxAmount = stack / chipValue;
+
+            return Random.Range(minAmount, maxAmount + 1);
+        }
+
+        public class ChipValueAmountPair
+        {
+            public int Value { get; }
+            public int Amount { get; }
+
+            public ChipValueAmountPair(int value, int amount)
+            {
+                Value = value;
+                Amount = amount;
+            }
+
+            public override string ToString()
+            {
+                return Value + " x" + Amount;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Table/StackDisplayer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace Table
+{
+    public class StackDisplayer : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text stackValueText;
+        [SerializeField] private GameObject chipsRoot;
+        
+        [SerializeField] private float widthSpacing;
+        [SerializeField] private float heightSpacing;
+
+        private const int MinStackChipCount = 10;
+        private const int MaxStackChipCount = 16;
+        private const string ChipSpritesPath = "Sprites/Chips";
+        private static Dictionary<int, Sprite> _chipValueToSprite;
+
+        public void Awake()
+        {
+            if (_chipValueToSprite != null) return;
+            
+            _chipValueToSprite = new Dictionary<int, Sprite>();
+            
+            foreach (var sprite in Resources.LoadAll<Sprite>(ChipSpritesPath))
+            {
+                sprite.texture.filterMode = FilterMode.Point;
+                _chipValueToSprite.Add(int.Parse(sprite.name), sprite);
+            }
+        }
+        
+        public void UpdateStack(int stack)
+        {
+            DeleteAllChips();
+
+            var chipDistribution = StackCalculator.CalculateChipDistribution(stack);
+            int sum = chipDistribution.Sum(pair => pair.Amount);
+
+            int stackChipCount = Random.Range(MinStackChipCount, MaxStackChipCount);
+            
+            int n = (int) Math.Ceiling((double) sum / stackChipCount);
+            float x = n % 2 == 0 ? -(n / 2 - 0.5f) * widthSpacing : -n / 2 * widthSpacing;
+            float y = 0f;
+            int chipCounter = 0;
+
+            foreach (var chipValueAmountPair in chipDistribution)
+            {
+                GameObject singleStackParent = new GameObject(chipValueAmountPair.Value.ToString());
+                singleStackParent.transform.SetParent(chipsRoot.transform, false);
+
+                for (int i = 0; i < chipValueAmountPair.Amount; i++)
+                {
+                    var chip = CreateChip(chipValueAmountPair.Value);
+
+                    chip.transform.SetParent(singleStackParent.transform, false);
+                    chip.transform.localPosition = new Vector3(x, y);
+
+                    y += heightSpacing;
+                    
+                    if (++chipCounter % stackChipCount == 0)
+                    {
+                        x += widthSpacing;
+                        y = 0f;
+                    }
+                }
+            }
+
+            stackValueText.text = stack.ToString();
+        }
+
+        private static GameObject CreateChip(int chipValue)
+        {
+            var chip = new GameObject("Chip " + chipValue);
+
+            Image image = chip.AddComponent<Image>();
+            image.sprite = _chipValueToSprite[chipValue];
+            image.SetNativeSize();
+
+            return chip;
+        }
+        
+        private void DeleteAllChips()
+        {
+            foreach (Transform child in chipsRoot.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
